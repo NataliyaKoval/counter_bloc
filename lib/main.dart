66dc8513387a71921734ecb00abc02d5f82159af
Nataly Koval <nataly.koval@n3wnormal.com>
2022-09-07 11:35:41 +0200

    create Second screen and anonymous route bloc access

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:counter_bloc/cubit/counter_cubit.dart';
+import 'package:counter_bloc/logic/cubit/counter_cubit.dart';
+import 'package:counter_bloc/presentation/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -12,96 +13,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<CounterCubit>(
-      create: (context) => CounterCubit(),
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: BlocProvider<CounterCubit>(
+          create: (context) => CounterCubit(),
+          child: const HomeScreen(title: 'Flutter Demo Home Page', color: Colors.blueAccent,)),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  // int _counter = 0;
-
-  // void _incrementCounter() {
-  //   setState(() {
-  //     _counter++;
-  //   });
-  // }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            BlocConsumer<CounterCubit, CounterState>(
-              listener: (context, state) => {
-                if (state.wasIncremented == true)
-                  {
-                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                      content: Text('Incremented!'),
-                      duration: Duration(milliseconds: 300),
-                    ))
-                  }
-                else if (state.wasIncremented == false)
-                  {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text('Decremented!'),
-                        duration: Duration(milliseconds: 300),
-                      ),
-                    )
-                  }
-              },
-              builder: (context, state) {
-                return Text(
-                  state.counterValue.toString(),
-                  style: Theme.of(context).textTheme.headline4,
-                );
-              },
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                FloatingActionButton(
-                  onPressed: () =>
-                      {BlocProvider.of<CounterCubit>(context).decrement()},
-                  tooltip: 'Decrement',
-                  child: const Icon(Icons.remove),
-                ),
-                FloatingActionButton(
-                  onPressed: () =>
-                      {BlocProvider.of<CounterCubit>(context).increment()},
-                  tooltip: 'Increment',
-                  child: const Icon(Icons.add),
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
